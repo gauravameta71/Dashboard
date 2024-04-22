c82707ee969d2f06daa4c5494429d5640c2ccbf3
gouravameta71@gmail.com <gouravameta71@gmail.com>
2024-04-22 17:51:30 +0530

    first commit

--- src/components/Charts/page.tsx
+++ src/components/Charts/page.tsx
@@ -2,7 +2,7 @@
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
 import ChartOne from "@/components/Charts/ChartOne";
 import ChartTwo from "@/components/Charts/ChartTwo";
-import ChartThree from "@/components/Charts/ChartThree";
+// import ChartThree from "@/components/Charts/ChartThree";
 import React from "react";
 
 const Chart: React.FC = () => {
@@ -13,7 +13,7 @@ const Chart: React.FC = () => {
       <div className="grid grid-cols-12 gap-4 md:gap-6 2xl:gap-7.5">
         <ChartOne />
         <ChartTwo />
-        <ChartThree />
+        {/* <ChartThree /> */}
       </div>
     </>
   );
